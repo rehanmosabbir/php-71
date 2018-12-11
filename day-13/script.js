c30470e6c257c01849ec64516a96485ec82f0594
rehanmosabbir <rehanmosabbir@gmail.com>
2018-12-11 14:37:05 +0600

    updatedxd

--- day-13/script.js
+++ day-13/script.js
@@ -1,18 +1,46 @@
 
 
+$('subImageOne').click(function () {
+    var getSubImage = this.attr('src');
+    $('mainImage').attr('src',getSubImage);
+});
+// function createElement() {
+//     var newHeadingElement = document.createElement('h1');
+//     var newParagraphElement = document.createElement('p');
+//     newHeadingElement.innerHTML = "This is a heading";
+//     newParagraphElement.innerHTML = "This is a paragraph.";
+//     document.getElementById('div').appendChild(newHeadingElement);
+//     document.getElementById('div').appendChild(newParagraphElement);
+// }
+//
+// document.getElementById('btn').onclick = function () {
+//     createElement();
+// };
 
-$('#firstName').keyup(function () {
-    var firstNameValue = $('#firstName').val();
-    $('#res1').text(firstNameValue);
-});
-$('#lastName').keyup(function () {
-    var lastNameValue = $('#lastName').val();
-    $('#res2').text(lastNameValue);
-});
 
-$('#lastName').blur(function () {
-    var firstNameValue = $('#firstName').val();
-    var lastNameValue = $('#lastName').val();
-    var fullNameValue = firstNameValue + ' ' + lastNameValue;
-    $('#res3').text(fullNameValue);
-});
+
+
+
+
+
+
+
+
+
+
+
+// $('#firstName').keyup(function () {
+//     var firstNameValue = $('#firstName').val();
+//     $('#res1').text(firstNameValue);
+// });
+// $('#lastName').keyup(function () {
+//     var lastNameValue = $('#lastName').val();
+//     $('#res2').text(lastNameValue);
+// });
+//
+// $('#lastName').blur(function () {
+//     var firstNameValue = $('#firstName').val();
+//     var lastNameValue = $('#lastName').val();
+//     var fullNameValue = firstNameValue + ' ' + lastNameValue;
+//     $('#res3').text(fullNameValue);
+// });
